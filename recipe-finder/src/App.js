60fc6fb1e3bd2238ac07695cfc2292740a5a317e
hitesh <hiteshpawar84@gmail.com>
2020-11-29 20:55:53 +0530

    initial commit

---
+++ recipe-finder/src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import {useEffect, useState , useRef} from "react";
+function App() {
+
+  const [ingredientList, updateIngredientList]= useState([]);
+  const [loading, setLoading] = useState(false);
+  const inputRef  = useRef(null);
+  const API_KEY = "29904352799874718cddacd6f77be909";
+  const APP_ID  = "6a43a79b";
+
+  const search = ()=>{
+    //console.log(inputRef);
+   SearchForRecipe(inputRef.current.value);
+   inputRef.current.value = "";
+  };
+ 
+
+
+  const SearchForRecipe = query => {
+
+    setLoading(true)
+    let url = `search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`;
+    fetch(url, {mode:"no-cors"}).then(response => {
+      return response.json();
+    })
+    .then(res=>{
+      console.log(res.hits);
+     updateIngredientList(res.hits);
+     setLoading(false);
+    })
+    .catch(err =>{
+      console.log("error",err)
+      setLoading(false);
+    });
+  }
+
+
+ useEffect(()=>{
+   SearchForRecipe(' ')
+  },[]);
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className="InputWrapper">
+     <input ref={inputRef} placeholder="Search for recipe"/>
+     <button onClick={search}>Search</button>
+      </div>
+      {loading && <p> Loading...</p>}
+
+         <div className= "wrapper">
+         {ingredientList.map(({recipe})=>{
+           const {label, image, ingredientLines} = recipe;
+            return(
+              <div key ={label}className="Ingredient">
+                 <span>{label}</span>
+                 <img src={image} alt="imag" />
+                
+                
+
+              </div>
+            )
+         })}
+
+         </div> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
